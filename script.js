c581260409a217213fa1927f7c5bf197574a3951
Vivaan-kumar12 <kumarvivan2024@gmail.com>
2026-07-17 07:54:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+// ================================
+// Realistic 3D Moon Project
+// Chapter 1 - Part 4
+// ================================
 
+// Create Scene
+const scene = new THREE.Scene();
+
+// Create Camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+// Camera Position
+camera.position.z = 5;
+
+// Create Renderer
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
+
+// Renderer Size
+renderer.setSize(
+    window.innerWidth,
+    window.innerHeight
+);
+
+// Pixel Ratio
+renderer.setPixelRatio(
+    Math.min(window.devicePixelRatio, 2)
+);
+
+// Background Color
+renderer.setClearColor(0x000000);
+
+// Add Canvas to Body
+document.body.appendChild(
+    renderer.domElement
+);
+
+// Render Scene
+renderer.render(scene, camera);
